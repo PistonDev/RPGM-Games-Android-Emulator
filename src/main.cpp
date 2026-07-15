4596a3a95c1e4d1f84687cf5bdb7f634f9a3be80
PistonDev <piston.dev.qzx@gmail.com>
2026-07-16 01:48:18 +0530

    work2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,37 @@
-#include <iostream>
+#include <raylib.h>
+
+class Emulator {
+public:
+	Emulator() {
+		InitWindow(800, 600, "RPGM Android Emulator");
+	}
+
+	void Update() {
+	}
+
+	void Draw() {
+
+	}
+
+	~Emulator() {
+		CloseWindow();
+	}
+};
 
 int main() {
-	std::cout << "hello" << std::endl;
+
+	Emulator emulator;
+
+	while (!WindowShouldClose()) {
+		emulator.Update();
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+
+		emulator.Draw();
+
+		EndDrawing();
+	}
 
 	return 0;
 }
